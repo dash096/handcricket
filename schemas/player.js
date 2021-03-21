26b5b1aca58e06d6207e4927e81fcf30b48c7fb0
Dash <dashskndash@gmail.com>
2021-03-21 16:07:18 +0530

    models

---
+++ schemas/player.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const player = new mongoose.Schema({
+  _id: { type: String, required: true },
+  cc: { type: Number, default: 500 },
+  wins: { type: Number, default: 0 },
+  loses: { type: Number, default: 0 },
+  goldMulti: { type: Number, default: 0 },
+  tossMulti: { type: Number, default: 0.5 },
+  bag: [],
+  level: { type: String, default: 'Starter' },
+  startedOn: { type: Date, required: true},
+  status: { type: Boolean, default: false },
+});
+
+const model = mongoose.model("player", player);
+
+module.exports = model
