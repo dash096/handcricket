0dc97a4b4735ff0aa6c9add6d049f19614807fd3
deadfool09 <dashskndash@gmail.com>
2021-03-22 18:58:53 +0530

    balance

---
+++ commands/balance.js
@@ -0,0 +1,22 @@
+const db = require('../schemas/player,js');
+
+module.exports = {
+  name: 'balance',
+  aliases: ['bal',
+    'cash',
+    'wallet'],
+  description: 'Shows balance of a user.',
+  category: 'handcricket',
+  cooldown: '15s',
+  run: async ({
+    message
+  }) => {
+    const target = message.mentions.members.first() || message.author;
+
+    const data = await db.findOne({
+      _id: target.id
+    });
+
+    message.channel.send(`**${target.username}** has ${data.coins} coins.`);
+  }
+}
