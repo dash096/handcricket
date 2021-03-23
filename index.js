60a6fa1aad99612913a07b5c8df4e76b5c716006
deadfool09 <dashskndash@gmail.com>
2021-03-23 14:04:00 +0530

    e

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("ready", () => {
 });
 
 client.on('message', async m => {
-  const db = require('./schemas.items.js');
+  const db = require('./schemas/items.js');
   
   const args = m.content.toLowerCase().trim().split(' ').slice(1);
   
