0ad5241799b10f2ec732061af77e56476b03e31d
deadfool09 <dashskndash@gmail.com>
2021-03-23 08:55:33 +0530

    e

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
 const Discord = require("discord.js");
 
 const client = new Discord.Client({
-  partials: ['USER', 'CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION']
+    partials: ['USER', 'CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION']
 });
 
 const WOKCommands = require("wokcommands");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const emojiGuild = client.guilds.fetch('823608260166025217');
-const emoji = emojiGuild.emojis.cache.find(emoji => emoji.name === 'cc');
-console.log(emoji);
-module.exports = emoji;
+getEmoji()
+
+async
+function getEmoji() {
+    const emojiGuild = client.guilds.fetch('823608260166025217');
+    const emoji = emojiGuild.emojis.cache.find(emoji => emoji.name === 'cc');
+    console.log(emoji);
+    module.exports = emoji;
+}
 
 client.on("ready", () => {
-  console.log("Logged in!");
-  client.user.setActivity("HandCricket");
+    console.log("Logged in!");
+    client.user.setActivity("HandCricket");
 
-  const dbOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }
-
-  mongoose.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }).catch (e => {
-    if (e) {
-      console.log(e);
+    const dbOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
     }
-    console.log("Mongo Connected");
-  });
 
-  const messagesPath = "";
+    mongoose.connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }).
+    catch (e => {
+        if (e) {
+            console.log(e);
+        }
+        console.log("Mongo Connected");
+    });
 
-  const disabledDefaultCommands = [];
+    const messagesPath = "";
 
-  new WOKCommands(client,
-    {
-      commandsDir: "commands",
-      featureDir: "features",
-      messagesPath,
-      showWarns: true,
-      dbOptions,
-      disabledDefaultCommands
+    const disabledDefaultCommands = [];
+
+    new WOKCommands(client, {
+        commandsDir: "commands",
+        featureDir: "features",
+        messagesPath,
+        showWarns: true,
+        dbOptions,
+        disabledDefaultCommands
     })
-  .setMongoPath(process.env.MONGO)
-  .setDefaultPrefix("!")
-  .setBotOwner("772368021821718549")
-  .setColor('#2d61b5')
-  .setCategorySettings([{
-    name: "general",
-    emoji: "👀"
-  },
-    {
-      name: "handcricket",
-      emoji: "🏏"
-    }]);
+        .setMongoPath(process.env.MONGO)
+        .setDefaultPrefix("!")
+        .setBotOwner("772368021821718549")
+        .setColor('#2d61b5')
+        .setCategorySettings([{
+            name: "general",
+            emoji: "👀"
+        }, {
+            name: "handcricket",
+            emoji: "🏏"
+        }]);
 
 });
 
