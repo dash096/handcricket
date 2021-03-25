2ea4727124ed428d2b91dc7d73e4c775761f5a30
deadfool09 <dashskndash@gmail.com>
2021-03-25 16:30:45 +0530

    shutdown command

---
+++ commands/shutdown.js
@@ -0,0 +1,14 @@
+module.exports => {
+  name: 'shutdown',
+  category: 'general',
+  hidden: true,
+  description: 'Owner Only'
+  run: ({message}) => {
+    if(message.author.id === '772368021821718549') {
+      process.exit();
+    }
+    else {
+      message.reply('Seriously You think you can do this?');
+    }
+  }
+}
