7eeb769e206b21dd9a670dc461fa9a74e2a7d0e7
deadfool09 <dashskndash@gmail.com>
2021-03-25 16:46:55 +0530

    e

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,11 +1,12 @@
 module.exports = {
-  name: 'shutdown',
+  name: 'restarted',
   category: 'general',
   hidden: true,
   description: 'Owner Only',
-  run: ({message}) => {
+  run: async ({message}) => {
     if(message.author.id === '772368021821718549') {
-      process.exit();
+      message.reply('Be right Back.');
+      await process.exit();
     }
     else {
       message.reply('Seriously You think you can do this?');
