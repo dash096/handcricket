0e5e8df1288f821fc94544388004ce8cef25227b
deadfool09 <dashskndash@gmail.com>
2021-03-27 09:07:28 +0530

    boosted emoji

--- commands/profile.js
+++ commands/profile.js
@@ -26,11 +26,11 @@ module.exports = {
     
     let cb = '';
     if(data.coinBoost) {
-      cb = 'boosted';
+      cb = ' ⏳';
     }
     let tb = '';
     if(data.tossBoost) {
-      tb = 'boosted';
+      tb = ' ⏳';
     }
     const embed = new Discord.MessageEmbed()
     .setTitle(`Profile of **${message.author.username}**`)
