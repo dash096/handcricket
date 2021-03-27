4cf95dc1de5282f91540c2b4d4d596c860310719
deadfool09 <dashskndash@gmail.com>
2021-03-27 14:23:11 +0530

    links

--- commands/links.js
+++ commands/links.js
@@ -5,11 +5,7 @@ module.exports = {
   description: 'Owner Only',
   run: async ({message}) => {
     if(message.author.id === '772368021821718549') {
-      message.channel.send(`
-      Github Repo: https://github.com/dash096/handcricket\n
-      Heroku Logs: https://dashboard.heroku.com/apps/handcricket/logs\n
-      Mongo Db: https://cloud.mongodb.com/v2/6050c5b487e707452e5775fc#metrics/replicaSet/6050c683609d0f72eb754ff4/explorer/myFirstDatabase/players/find
-      `)
+      message.channel.send(`Github Repo: https://github.com/dash096/handcricket\nHeroku Logs: https://dashboard.heroku.com/apps/handcricket/logs\nMongo Db: https://cloud.mongodb.com/v2/6050c5b487e707452e5775fc#metrics/replicaSet/6050c683609d0f72eb754ff4/explorer/myFirstDatabase/players/find`)
     }
   }
 }
