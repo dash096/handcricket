e809cdc1f24b162dead1d4e4bfb581e5308dbbd7
deadfool09 <dashskndash@gmail.com>
2021-03-27 12:37:41 +0530

    links fil

---
+++ commands/links.js
@@ -0,0 +1,18 @@
+ymodule.exports = {
+  name: 'restart',
+  category: 'general',
+  hidden: true,
+  description: 'Owner Only',
+  run: async ({message}) => {
+    if(message.author.id === '772368021821718549') {
+      message.channel.send(`
+      Github Repo: https://github.com/dash096/handcricket\n
+      Heroku Logs: https://dashboard.heroku.com/apps/handcricket/logs\n
+      Mongo Db: https://cloud.mongodb.com/v2/6050c5b487e707452e5775fc#metrics/replicaSet/6050c683609d0f72eb754ff4/explorer/myFirstDatabase/players/find
+      `}
+    }
+    else {
+      message.reply('Seriously You think you can do this?');
+    }
+  }
+}
