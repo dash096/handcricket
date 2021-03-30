e70aba9f32bde6982ce9eac9d46c8bff461be64d
deadfool09 <dashskndash@gmail.com>
2021-03-30 12:31:02 +0530

    add check

--- functions/gainExp.js
+++ functions/gainExp.js
@@ -4,7 +4,7 @@ module.exports = async function (a, amount) {
   const oldxp = a.xp;
   const add = Math.random() * amount;
   console.log(add);
-  await a.findOneAndUpdate({_id: a.id},
+  await db.findOneAndUpdate({_id: a.id},
     {
       $set: {
         xp: oldxp + add
