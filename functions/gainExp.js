9ff9482d0cf77191c807e16b25d054b537777c5a
deadfool09 <dashskndash@gmail.com>
2021-03-30 12:27:54 +0530

    check add

--- functions/gainExp.js
+++ functions/gainExp.js
@@ -3,6 +3,7 @@ const db = require('../schemas/player.js');
 module.exports = async function (a, amount) {
   const oldxp = a.xp;
   const add = Math.random() * amount;
+  console.log(add);
   await a.findOneAndUpdate({_id: a.id},
     {
       $set: {
