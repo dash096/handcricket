47dda5fd76402474916515da843290fd22e3185e
deadfool09 <dashskndash@gmail.com>
2021-04-13 21:40:44 +0530

    gib tracks when an user reaches level 0

--- functions/gainExp.js
+++ functions/gainExp.js
@@ -25,6 +25,12 @@ module.exports = async function (nabData, amt, msg, user) {
     if(user) msg.send(`${user} CONGRATS!!! You leveled up to **${sLevel}**! You also got a lootbox!!!`);
     else msg.reply(`CONGRATS!!! You leveled up to **${sLevel}**! You also got a lootbox!!!`);
     await updateBag('lootbox', -1, data, msg);
+    if(sLevel === 0) { //If the level is 0, gib tracks
+      const decors = user.decors || {};
+      const tracks = decors.tracks || 0;
+      decors.tracks = tracks + 1;
+      await db.findOneAndUpdate({_id: data._id}, {$set: {decors: decors}}});
+    }
     await db.findOneAndUpdate({_id: data._id}, {$set: {xp: sXP + 1}});
     return;
   }
