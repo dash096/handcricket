5f96ab0f33aeba13b90a63922c67cb1e1eacd5a2
deadfool09 <dashskndash@gmail.com>
2021-04-14 23:55:34 +0530

    no need to pong, sensitivity decreased

--- functions/getTarget.js
+++ functions/getTarget.js
@@ -1,13 +1,12 @@
 module.exports = (message, args, client) => {
   let target;
   if(args.length > 0) {
-    let query = args[0].toLowerCase();
+    let query = args[0];
     target = message.mentions.users.first() || 
     client.users.cache.get(query) || 
-    client.users.cache.find(user => user.tag.toLowerCase().startsWith(query)) ||
-    (message.guild.members.cache.find(member => member.displayName.toLowerCase().startsWith(query))).user;
+    client.users.cache.find(user => user.tag == query);
     if(!target || target.bot) {
-      message.reply('Invalid user. Nice Try.');
+      message.reply('Invalid Target, Silly. You should do one of these for a non-bot user. Either ping them, mention the id or type their @username#0000 and remove the @');
       return;
     }
     return target;
