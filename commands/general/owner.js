d7178ae4aa44a77449fd6076aa3db344f0afb9c3
deadfool09 <dashskndash@gmail.com>
2021-04-16 12:59:26 +0530

    see the owner

---
+++ commands/general/owner.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'ping',
+    description: 'Pong!',
+    category: 'General',
+    syntax: 'e.ping',
+    cooldown: 2,
+    run: async ({message, client}) => {
+      const { content, author, channel, mentions } = message;
+      const user = await client.users.fetch('772368021821718549');
+      channel.send('The owner of the bot is ' + user.tag);
+    }
+};
