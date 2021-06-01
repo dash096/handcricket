2f992c40eb9771919d7210fbf5419c60fb2e9008
dash096 <dashskndash@gmail.com>
2021-06-01 11:03:23 +0530

    express

--- features/checkVotes.js
+++ features/checkVotes.js
@@ -1,76 +1,24 @@
 const db = require('../schemas/player.js');
 const getEmoji = require('../index.js');
 const getDecors = require('../functions/getDecors.js');
+const express = require('express')
+const { Webhook } = require('@top-gg/sdk')
 
 module.exports = async ({client, topggapi}) => {
   let votes = await topggapi.getVotes();
   
-  setInterval(async () => {
-    const newVotes = await topggapi.getVotes();
-    
-    let count = {};
-    
-    for (const vote of votes) {
-      if(count[vote.id]) {
-        count[vote.id] += 1;
-      } else {
-        count[vote.id] = 1;
-      }
-    }
-    for (const newVote of newVotes) {
-      if(count[newVote.id]) {
-        count[newVote.id] += 1;
-      } else {
-        count[newVote.id] = 1;
-      }
-    }
-    
-    let newVotesArray = Object.keys(count).filter(key => count[key] % 2 === 1);
-    
-    if(!newVotesArray || newVotesArray.length === 0) {
-      votes = newVotes;
-      return;
-    }
-    
-    for (var i = 0; i < newVotesArray.length; i++) {
-      try {
-        const user = await client.users.fetch(newVotesArray[i]);
-        if((await topggapi.hasVoted(user.id)) === false) return;
-        const cooldown = Date.now() + (60 * 60 * 12 * 1000);
-        const data = await db.findOne({ _id: user.id });
-        await user.send('Thanks for voting, you got ' + await rewards(data, user));
-        let quests = data.quests || {};
-        let streak = data.voteStreak || 0;
-        quests.support = true;
-        streak += 1;
-        await db.findOneAndUpdate({ _id: user.id }, {
-          $set: {
-            voteClaim: true,
-            voteCooldown: cooldown,
-            quests: quests,
-            voteStreak: streak,
-            lastVoted: Date.now()
-          }
-        });
-        setTimeout(async () => {
-          await db.findOneAndUpdate({ _id: user.id }, {
-            $set: {
-              voteClaim: false,
-            },
-            $unset: {
-              voteCooldown: false,
-            }
-          });
-        }, 60 * 60 * 12 * 1000);
-      } catch (e) {
-        console.log(e);
-        votes = newVotes;
-        return;
-      }
-    }
-    votes = newVotes;
-  }, 60 * 2.5 * 1000); //2 and half minutes
+  const wh = new Webhook(process.env.TOPGG_WEBHOOK_PASSWORD)
+  const app = express()
   
+  app.get('/', (req, res) => {
+    res.send('<h2> Hello World </h2>')
+  })
+  
+  app.post('/webhook', wh.listener((vote) => {
+    console.log(vote.user) // 172075838806818817
+  }))
+  
+  app.listen(process.env.PORT);
 };
 
 async function rewards(data, user) {
