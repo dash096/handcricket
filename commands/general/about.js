fdcf4f1e88686565b3cb98ba55a7ccbac56828b0
dash096 <dashskndash@gmail.com>
2021-06-10 15:35:32 +0530

    about command changes

--- commands/general/about.js
+++ commands/general/about.js
@@ -13,12 +13,17 @@ module.exports = {
     let RAM = Math.round( ((process.memoryUsage().heapUsed) /1024 /1024 * 100) / 100) + ' MB';
     let GUILDS = client.guilds.cache.size;
     let USERS = (await db.find()).length;
+    let CACHED_USERS = client.users.cache.size;
     let TIME = parseInt((process.uptime())/60) + ' MINS';
     const embed = new Discord.MessageEmbed()
       .setTitle('About Dispo Cheems')
       .setDescription('Thanks to Aadithya, General Nix Sh Sandile, Diego, Killua, Ace, Basara, Prateek and all other peeps who helped the bot grow and test it out.')
       .addField('Links', '[Add the bot](https://bit.ly/dispo-bot)\n[Vote me](https://top.gg/bot/804346878027235398/vote)\n[Support Server](https://bit.ly/dispoGuild)')
-      .addField('Stats', `${GUILDS} Guilds\n${USERS} Players`)
+      .addField('Guilds', `${GUILDS} Guilds`)
+      .addField(
+        'Users',
+        `Unique Users: ${USERS} users\NCached Users: ${CACHED_USERS} users`
+      )
       .addField('Process', `Ram Usage: ${RAM}\nUptime: ${TIME}`)
       .addField('🔫 Dispo Suicide Squad 🔫', `Dash\`#7374\`\nUltraMoonEagle\`#3876\`\nKing Kane\`#5483\`\nGRENINJA\`#9537\`\nPanda_Rose\`#1331\``)
       .setFooter('Made with 💖 with discord.js', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6B6HUVgdNO6fZQYdFl5GvhMPc1B5_LXjjPDpgTZML7DWZvneIJz7tUc&s=10")
