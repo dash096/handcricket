3318c696c120ab38191717e88e87a43fa2889541
dash096 <dashskndash@gmail.com>
2021-06-11 17:19:09 +0530

    server wrapper

--- server/main.js
+++ server/main.js
@@ -3,117 +3,114 @@ var html = `
 
 <head>
   <title> Dispo </title>
+  
   <style>
       ${require('./main.css')}
   </style>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-  <div id='wrapper'>
-  
 
-  <div id='header'>
-      <h2>
-          Under Construction, Mobile only.
-      </h2>
-  </div>
+  <div id='wrapper'>
+    
+    <div id='header'>
+        <h2>
+            Under Construction, Mobile only.
+        </h2>
+    </div>
+
+    
+    <div id='design'>
+    </div>
+
+    
+    <div id='main'>
+        <div id='logo_container'>
+            <img src='${process.env.AVATAR_URL}' alt='logo'>
+        </div>
+
+        <h3 id='title'> Dispo </h3>
+        <p>
+          The gateway to online sporting
+        </p>
+
+        <div class='redirect'>
+            <a href="${process.env.INVITE_URL}"> <strong> Invite Bot </strong> </a>
+        </div>
+    </div>
   
-  <div id='design'>
-  </div>
   
-  <div id='main'>
-      <div id='logo_container'>
-          <img src='${process.env.AVATAR_URL}' alt='logo'>
-      </div>
+    <div id='features'>
+        <div id='title'>
+            <h3>
+              Why Dispo?
+            </h3>
+        </div>
       
-      <h3 id='title'> Dispo </h3>
-      <p>
-        The gateway to online sporting
-      </p>
-      
-      <div class='redirect'>
-          <a href="${process.env.INVITE_URL}"> <strong> Invite Bot </strong> </a>
-      </div>
-      
-  </div>
-  
-  
-  
-  
-  <div id='features'>
-      
-      <div id='title'>
-          <h3>
-            Why Dispo?
-          </h3>
-      </div>
-      
-      <div id='cricket' class='category'>
-          <img src='https://i.postimg.cc/FKGdD7zb/cricket-player.png'>
+        <div id='cricket' class='category'>
+            <img src='https://i.postimg.cc/FKGdD7zb/cricket-player.png'>
+            
+            <h4> Cricket </h4>
+            <p>
+                The sport cricket in Discord with actual strikeRate, runRate, orangeCap holder, purpleCap holder, total runs, total wickets with some powerups together brings you real fun with your friends!
+            </p>
+        </div>
+        <div id='football' class='category'>
+            <img src='https://i.postimg.cc/pdP5fDWX/football-players.png'>
+           
+            <h4> Football </h4>
+            <p>
+                Get your hands on the most interesting part of Football, Penalty Shootouts!
+            </p>
+        </div>
+        <div id='games' class='category'>
+            <img src='https://i.postimg.cc/HWzMHKkB/joystick.png'>
           
-          <h4> Cricket </h4>
-          <p>
-              The sport cricket in Discord with actual strikeRate, runRate, orangeCap holder, purpleCap holder, total runs, total wickets with some powerups together brings you real fun with your friends!
-          </p>
-      </div>
-      
-      <div id='football' class='category'>
-          <img src='https://i.postimg.cc/pdP5fDWX/football-players.png'>
-          
-          <h4> Football </h4>
-          <p>
-              Get your hands on the most interesting part of Football, Penalty Shootouts!
-          </p>
-      </div>
-      
-      <div id='games' class='category'>
-          <img src='https://i.postimg.cc/HWzMHKkB/joystick.png'>
-          
-          <h4> Minigames </h4>
-          <p>
-              We also found a way to cure your loneliness, try the minigames also known as Single Player Games.
-          </p>
-      </div>
-      
-  </div>
+            <h4> Minigames </h4>
+            <p>
+                We also found a way to cure your loneliness, try the minigames also known as Single Player Games.
+            </p>
+        </div>
+    </div>
   
   
-  <div id='joinus'>
-      <div id='title'>
-          <h3>
-          Community
-          </h3>
-      </div>
-      
-      <div id='description'>
-          <p>
-          Excited? Want to try it? Join our community server, let us have fun together!
-          </p>
-          <br>
-          <strong> Community Perks: </strong>
-          
-          <ul>
-              <li> Helmet for your character
-              <li> 2x Coin Multi
-              <li> Access to Decor Shop
-              <li> More hidden stuffs
-          </ul>
-      </div>
-      
-      <div id='redirect'>
-          <a href="${process.env.COMMUNITY_URL}">
-              Join us
-          </a>
-      </div>
-  </div>
+    <div id='joinus'>
+        <div id='title'>
+            <h3>
+            Community
+            </h3>
+        </div>
+        <div id='description'>
+            <p>
+                Excited? Want to try it? Join our community server, let us have fun together!
+            </p>
+            <br>
+            <strong> Community Perks: </strong>
+            
+            <ul>
+                <li> Helmet for your character
+                <li> 2x Coin Multi
+                <li> Access to Decor Shop
+                <li> More hidden stuffs
+            </ul>
+        </div>
+        <div id='redirect'>
+            <a href="${process.env.COMMUNITY_URL}">
+                Join us
+            </a>
+        </div>
+    </div>
   
   
-  <div id='footer'>
-      <h2> @Dispo </h2>
-  </div>
+    <div id='footer'>
+        <h2> @Dispo </h2>
+    </div>
   
   
   </div>
+  
 </body>
 
 </html>
