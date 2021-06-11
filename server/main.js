1b685f9f71bcf5cad66126295ede77726505e31b
dash096 <dashskndash@gmail.com>
2021-06-11 17:40:55 +0530

    nav bar

--- server/main.js
+++ server/main.js
@@ -8,7 +8,6 @@ var html = `
       ${require('./main.css')}
   </style>
   
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
@@ -16,9 +15,14 @@ var html = `
   <div id='wrapper'>
     
     <div id='header'>
-        <h2>
-            Under Construction, Mobile only.
-        </h2>
+        <div id='nav_icon_container'>
+            <div class='nav_icon' id='one'>
+            </div>
+            <div class='nav_icon' id='two'>
+            </div>
+            <div class='nav_icon' id='three'>
+            </div>
+        </div>
     </div>
 
     
