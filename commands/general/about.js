94afd927cbfbd946716056464dbff29c69e8cceb
dash096 <dashskndash@gmail.com>
2021-06-12 22:46:18 +0530

    active users on about command

--- commands/general/about.js
+++ commands/general/about.js
@@ -14,6 +14,7 @@ module.exports = {
     let GUILDS = client.guilds.cache.size;
     let USERS = (await db.find()).length;
     let CACHED_USERS = client.users.cache.size;
+    let ACTIVE_USERS = (await db.find({ status: true })).length;
     let TIME = parseInt((process.uptime())/60) + ' MINS';
     
     const embed = new Discord.MessageEmbed()
@@ -23,10 +24,10 @@ module.exports = {
       .addField('Guilds', `${GUILDS} Guilds`)
       .addField(
         'Users',
-        `Unique Users: ${USERS} users\nCached Users: ${CACHED_USERS} users`
+        `Unique Users: ${USERS} users\nCached Users: ${CACHED_USERS} users\nPlaying Now: ${ACTIVE_USERS} users`
       )
       .addField('Process', `Ram Usage: ${RAM}\nUptime: ${TIME}`)
-      .addField('🔫 Dispo Suicide Squad 🔫', `Dash\`#0966\`\nUltraMoonEagle\`#3876\`\nKing Kane\`#5483\`\nGRENINJA\`#9537\`\nPanda_Rose\`#1331\`, Discriminators might be invalid!`)
+      .addField('🔫 Dispo Suicide Squad 🔫', `Dash\`#0966\`\nUltraMoonEagle\`#3876\`\nKing Kane\`#5483\`\nGRENINJA\`#9537\`\nPanda_Rose\`#1331\`,\nDiscriminators might be invalid!`)
       .setFooter('Made with 💖 with discord.js', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6B6HUVgdNO6fZQYdFl5GvhMPc1B5_LXjjPDpgTZML7DWZvneIJz7tUc&s=10")
       .setColor(embedColor);
     
