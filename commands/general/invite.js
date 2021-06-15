443d692e6cf5a65fda3ae8f48e54bc2e221cb4c0
dash096 <dashskndash@gmail.com>
2021-06-15 21:44:02 +0530

    invite command for server

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -3,13 +3,22 @@ const embedColor = require('../../functions/getEmbedColor.js');
 
 module.exports = {
   name: 'invite',
-  aliases: ['link', 'support', 'server', 'join', 'bot', 'add'],
+  aliases: ['support', 'server', 'join', 'bot', 'add'],
   description: 'Invite the Bot or Join the Official server',
   category: 'General',
   syntax: 'e.invite',
   cooldown: 2,
-  run: ({message}) => {
-    const { content, author, channel, mentions } = message;
-    message.reply('https://discord.com/api/oauth2/authorize?client_id=804346878027235398&permissions=321600&scope=bot');
+  run: ({message, prefix}) => {
+    let { content, author, channel, mentions } = message;
+    
+    let command = content.slice(prefix.length).trim().split(/ +/).shift().toLowerCase();
+    
+    let serverAlias = ['support', 'community', 'server', 'join']
+    
+    if (serverAlias.includes(command)) {
+      message.reply('discord.gg/3dhtFggFXZ');
+    } else {
+      message.reply('https://discord.com/api/oauth2/authorize?client_id=804346878027235398&permissions=321600&scope=bot');
+    }
   }
 };
