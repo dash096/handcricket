753c6e22ce031680fb1578388126f20e3a2e9cf2
dash096 <dashskndash@gmail.com>
2021-07-01 18:53:30 +0530

    equip fixes

--- commands/dogenomy/equip.js
+++ commands/dogenomy/equip.js
@@ -51,7 +51,7 @@ module.exports = {
       }
     }
     
-    if(userHasDecor.includes(decor)) {
+    if(userDecors.includes(decor)) {
       equipped.splice(equipped.indexOf(decor), 1);
       await db.findOneAndUpdate({_id: data.id}, {$set: {decors: userDecors}});
       message.reply('You removed ' + args.join(' '));
