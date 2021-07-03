a196b8e0880d01ccd3e845355246db4affa96d68
dash096 <dashskndash@gmail.com>
2021-07-03 20:43:33 +0530

    gCreate fixes

--- features/guildCreate.js
+++ features/guildCreate.js
@@ -1,17 +1,29 @@
 const ServerID = process.env.SERVERID
 
+
+
+
+
+
 module.exports = async ({ client }) => {
-  client.on('guildCreate',async (guild) => {
-    let mems = Array.from(guild.members.cache).map(m => m[1].user.bot)
-    
+  client.on('guildCreate', async (guild) => {
     let bots = 0
     let humans = 0
     
-    let i = 0
-    for (i; i < mems.length; i++) {
-      mem = mems[i]
-      if (mem === true) bots += 1
+    let members = (await guild.members.fetch()).map(m => m)
+    let bots = 0
+    let humans = 0
+    for (let i = 0; i < members.length; i++) {
+      if (members[i].user.bot) bots += 1
       else humans += 1
+    }
+    
+    if (bots >= humans) {
+      await guild.owner.user.send('Seems like too many bots as compared to humans in the server, I left it...')
+    } else if (bots > 75) {
+      await guild.owner.user.send('Oof, 50+ bots in your server, seems like a bot farm, I left it.')
+    } else if (humans <= 2) {
+      await guild.owner.user.send('I left the server as it was detected private.')
     }
     
     let user = await client.users.fetch(guild.ownerID)
