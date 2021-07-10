7e3b031720265a7457d8660290c5f9052f0a88ee
dash096 <dashskndash@gmail.com>
2021-07-10 09:45:36 +0530

    fixes

--- cardFunctions/buySlots.js
+++ cardFunctions/buySlots.js
@@ -6,16 +6,22 @@ const db = require('../schemas/player.js')
 const updateCoins = require('../functions/updateCoins.js')
 
 module.exports = async (data, amount) => {
-  let slots = data?.cards?.[0]?.slots || 10
+  let cards = data.cards || []
+  
+  let slots = cards[0]?.slots || 10
   let price = 0
   for (let i = 0; i < amount; i++) {
     slots += 1
     price += slots ** 2 * 10
   }
-  await updateCoins(price, data)
+  
+  await updateCoins(-(price), data)
   await db.findOneAndUpdate({ _id: data._id }, {
     $set: {
-      "cards.$0.slots": slots
+      "cards": [{
+        team: cards.team || [],
+        slots: slots,
+      }, ...cards.slice(1)]
     }
   })
 }
