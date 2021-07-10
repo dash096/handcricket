1f1a1402724e99d190b7671d699839b8d9a81bba
dash096 <dashskndash@gmail.com>
2021-07-10 17:00:12 +0530

    fixes

--- cardFunctions/updateCards.js
+++ cardFunctions/updateCards.js
@@ -4,6 +4,8 @@ module.exports = async (data, card, remove) => {
   let { fullname } = card
   let cards = data.cards || {}
   
+  
+  console.log(data, card, cards, remove)
   if ( !remove && data.cards[0].slots || 10 <= data.cards.slice(1).length - 1 ) {
     return 'err'
   }
