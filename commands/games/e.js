e0ddfdbf63e5f98956ba0796e6fd2695b6d1319b
dash096 <dashskndash@gmail.com>
2021-07-11 16:41:41 +0530

    fixes

--- commands/games/e.js
+++ commands/games/e.js
@@ -11,16 +11,22 @@ module.exports = {
   name: 'e',
   run: async ({ message, args }) => {
     let name = args[0] || 'kohli'
+    
     let row = args[1] || 1
     let col = args[2] || 1
+    let xpx = teamPos['x'][row][col]
+    let ypx = teamPos[row]
+    
     let path = `./assets/cards/${name}.png`
     let bg = './assets/team11.jpg'
     
     let bgImg = await jimp.read(bg)
     let cardImg = await jimp.read(path)
     await bgImg
-          .composite(cardImg, teamPos['x'][row][col], teamPos['y'][row])
+          .composite(await cardImg, xpx, ypx)
           .write('./temp/i.png')
+    
+    console.log(xpx, ypx)
     
     await new Promise(r => setTimeout(r, 2000))
     await message.channel.send('text', {
