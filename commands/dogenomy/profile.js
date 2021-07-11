3ad2cf164d9b5881855522fb34843106e0cea02a
dash096 <dashskndash@gmail.com>
2021-07-11 20:21:59 +0530

    fixes

--- commands/dogenomy/profile.js
+++ commands/dogenomy/profile.js
@@ -45,8 +45,7 @@ module.exports = {
     const WR = getWR(data);
     const orangeCaps = data.orangeCaps || 0;
     
-    const characterPath = await new Promise(r => { return getCharacter(target, r) })
-    console.log('after')
+    const characterPath = await new Promise(async r => { return await getCharacter(target, r) })
     const characterAttachment = new Discord.MessageAttachment(characterPath);
     
     let description = userInfo();
@@ -214,7 +213,6 @@ async function getCharacter(target, resolve) {
     });
   }
   const image = await getImage(target, type, images)
-  console.log('before', resolve)
   return resolve(image)
 }
 
