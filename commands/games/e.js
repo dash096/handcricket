c38e94922d500912197d5b9a2727e7e93b81d483
dash096 <dashskndash@gmail.com>
2021-07-11 16:44:11 +0530

    fixes

--- commands/games/e.js
+++ commands/games/e.js
@@ -15,7 +15,9 @@ module.exports = {
     let row = args[1] || 1
     let col = args[2] || 1
     let xpx = teamPos['x'][row][col]
-    let ypx = teamPos[row]
+    let ypx = teamPos['y'][row]
+    
+    console.log(xpx, ypx)
     
     let path = `./assets/cards/${name}.png`
     let bg = './assets/team11.jpg'
@@ -25,8 +27,6 @@ module.exports = {
     await bgImg
           .composite(await cardImg, xpx, ypx)
           .write('./temp/i.png')
-    
-    console.log(xpx, ypx)
     
     await new Promise(r => setTimeout(r, 2000))
     await message.channel.send('text', {
