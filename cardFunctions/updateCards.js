dd18908d3b2a3b951c1f47b341629d1f3d8b23b7
dash096 <dashskndash@gmail.com>
2021-07-12 23:18:46 +0530

    fixes

--- cardFunctions/updateCards.js
+++ cardFunctions/updateCards.js
@@ -21,7 +21,7 @@ module.exports = async (data, card, mode, remove, add = []) => {
   }
   
   Promise.all(
-    [await db.findOneAndUpdate({ _id: data._id, $cards: { $function: { body: function() { return true }, lang: 'js' } } }, {
+    [await db.findOneAndUpdate({ _id: data._id, cards: { $exist: false } }, {
       $set: (mode === 'team11' 
           ? {
             "cards.$": {
