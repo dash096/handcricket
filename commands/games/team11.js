311e28ddd16da092a9a6296c76cd36a022c4759f
dash096 <dashskndash@gmail.com>
2021-07-12 20:43:48 +0530

    fixes

--- commands/games/team11.js
+++ commands/games/team11.js
@@ -27,7 +27,7 @@ module.exports = {
     if (!data.cards?.[0]?.team || data.cards[0].team.length < 11) {
       for (let i = 0; i < 11; i++) {
         let card = await openBox(1, data, message, 'cricket', -75)
-        await updateCard(data, card, 'team11', -75)
+        await updateCard(data, card, 'team11')
         data = await db.findOne({ _id: target.id })
       }
       await message.reply('You have been given 11 starter cards!')
