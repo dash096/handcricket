d5a6593a1b89ae4149e41f7ab595491949bb5121
dash096 <dashskndash@gmail.com>
2021-07-14 11:29:13 +0530

    fixes

--- commands/games/team11.js
+++ commands/games/team11.js
@@ -48,8 +48,8 @@ module.exports = {
     if (replaceAlias.includes(args[0]) || swapAlias.includes(args[0])) {
       if (args.length < 3) return message.reply(getError({ error: 'syntax', filePath: 'games/team11.js' }))
       
-      let toReplace = await cardSearch([args[1]])
-      let toBeReplaced = await cardSearch([args[2]])
+      let toReplace = await cardSearch(args[1])
+      let toBeReplaced = await cardSearch(args[2])
       // Card Existence Validation
       if (!toReplace || !toBeReplaced) return message.reply(`Cannot find card: \`${toReplace ? args[1] : args[2]}\``)
       
