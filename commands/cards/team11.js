49082b951da7ccc93781ff3010c969315cc79734
dashskndash@gmail.com <dashskndash@gmail.com>
2022-04-11 09:07:18 +0530

    starters id only

--- commands/cards/team11.js
+++ commands/cards/team11.js
@@ -30,9 +30,9 @@ module.exports = {
     if (!(data.cards?.[0]?.team) || data.cards[0].team.length < 11) {
       let starters = await openBox(11, data, message, 'cricket', -75)
       Promise.all([
-        await updateCard(data, starters, 'team11')
+        await updateCard(data, starters.map(x => x._id), 'team11')
       ])
-      await updateCard(data, starters, 'slots')
+      await updateCard(data, starters.map(x => x._id), 'slots')
       await message.reply('You have been given 11 starter cards!')
       return
     }
